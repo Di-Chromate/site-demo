76ee36da3f117cc860f2c6d86369ccbdbf7a60c1
Di-Chromate <nicholaslockett96@yahoo.com>
2026-03-18 18:40:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// NAV BAR STYLING ON SCROLL
+
+const primaryHeader = document.querySelector('.primary-header')
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+primaryHeader.before(scrollWatcher);
+
+const navObserver= new IntersectionObserver((entries) => {
+  primaryHeader.classList.toggle('sticking', !entries[0].isIntersecting)
+});
+
+navObserver.observe(scrollWatcher)
